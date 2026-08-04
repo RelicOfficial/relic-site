3a2cd94d3643d7c77f90f97dac8f7e70e50fea93
RelicOfficial <wavesless07@gmail.com>
2026-08-04 15:16:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+window.addEventListener("scroll", () => {
 
+    const hero = document.querySelector(".hero");
+
+    if (window.scrollY > 50) {
+        hero.classList.add("hero-move");
+    } else {
+        hero.classList.remove("hero-move");
+    }
+
+});
