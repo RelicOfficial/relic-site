7ed02584478d3bbbcd9d4f777e71a9a463e20354
RelicOfficial <wavesless07@gmail.com>
2026-08-04 21:11:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let lastScroll = 0;
 const hero = document.querySelector(".hero");
+const heroShadow = document.querySelector(".hero-shadow");
 
 window.addEventListener("scroll", () => {
 
     if (window.scrollY > 100) {
         hero.classList.add("hero-move");
-    } 
-    else {
+        heroShadow.classList.add("hero-shadow-move");
+    } else {
         hero.classList.remove("hero-move");
+        heroShadow.classList.remove("hero-shadow-move");
     }
 
 });
